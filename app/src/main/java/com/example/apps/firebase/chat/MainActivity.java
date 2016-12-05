662e6425b7a39ca813f75777aca7ab4929ee7766
saikiapriyam <priyamsaikia.aec@gmail.com>
2016-12-05 17:34:08 +0530

    feat: remote config implemented

--- app/src/main/java/com/example/apps/firebase/chat/MainActivity.java
+++ app/src/main/java/com/example/apps/firebase/chat/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -17,6 +18,8 @@ import com.example.apps.firebase.chat.adapter.MessageAdapter;
 import com.example.apps.firebase.chat.beans.ChatMessageBean;
 import com.example.apps.firebase.chat.utils.AppConstants;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -24,14 +27,19 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String DEAFAULT_MSG_LENGTH = "DEFAULT_CHAR_COUNT";
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Bind(R.id.tv_error_rv)
     TextView tv_error_rv;
     @Bind(R.id.progress_bar)
@@ -53,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mFirebaseAuth;
     FirebaseAuth.AuthStateListener mAuthStateListener;
+    FirebaseRemoteConfig mFirebaseRemoteConfig;
+
+    long DEFAULT_CHARACTER_COUNT = 140;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,19 +98,44 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
-                            .setProviders(AuthUI.GOOGLE_PROVIDER).build(),
+                                    .setProviders(AuthUI.GOOGLE_PROVIDER).build(),
                             AppConstants.RC_SIGNIN);
                 }
             }
         };
 
+        mFirebaseRemoteConfig=FirebaseRemoteConfig.getInstance();
+        Map<String, Object> defultConfigMap = new HashMap<>();
+        defultConfigMap.put(DEAFAULT_MSG_LENGTH, DEFAULT_CHARACTER_COUNT);
+        fetchConfig();
+
     }
+
+    private void fetchConfig() {
+        mFirebaseRemoteConfig.fetch(0).addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void v) {
+                        mFirebaseRemoteConfig.activateFetched();
+                        long msg_length = mFirebaseRemoteConfig.getLong(DEAFAULT_MSG_LENGTH);
+                        DEFAULT_CHARACTER_COUNT = msg_length;
+                    }
+                }
+
+        ).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                long msg_length = mFirebaseRemoteConfig.getLong(DEAFAULT_MSG_LENGTH);
+                DEFAULT_CHARACTER_COUNT = msg_length;
+            }
+        });
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==AppConstants.RC_SIGNIN){
-            if(requestCode==RESULT_CANCELED)
+        if (resultCode == AppConstants.RC_SIGNIN) {
+            if (requestCode == RESULT_CANCELED)
                 finish();
         }
     }
@@ -163,7 +200,11 @@ public class MainActivity extends AppCompatActivity {
 
         if (TextUtils.isEmpty(editText.getText()))
             return;
-        else
+        else if (editText.getText().length() > DEFAULT_CHARACTER_COUNT) {
+            Log.d(TAG,"length = "+editText.getText().toString().length()+" allowed length = "+DEFAULT_CHARACTER_COUNT);
+            Toast.makeText(this, getText(R.string.error_long_msg), Toast.LENGTH_SHORT).show();
+            return;
+        } else
             sendMessage(editText);
     }
 
